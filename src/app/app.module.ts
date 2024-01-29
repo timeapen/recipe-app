02328677af574906f77ec7cd13a0e8794b03f518
Tim <tim.eapen@gmail.com>
2024-01-28 19:40:31 -0500

    242. making RecipeService a singleton and unsubscribing in recipe list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,8 @@ import { DropdownDirective } from './shared/dropdown.directive';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+import { ShoppingListService } from './shopping-list/shopping-list.service';
+import { RecipeService } from './recipes/recipe.service';
 
 @NgModule({
   declarations: [
@@ -35,7 +37,7 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
     ReactiveFormsModule,
     AppRoutingModule    
   ],
-  providers: [],
+  providers: [ShoppingListService, RecipeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,21 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { RecipeService } from '../recipe.service';
 import { Recipe } from '../recipes.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent implements OnInit, OnDestroy {
 
+  recipesChanged: Subscription;
   recipes: Recipe[];
 
   constructor(private recipeService: RecipeService, private activatedRoute: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
-    this.recipeService.recipesChanged
+    this.recipesChanged = this.recipeService.recipesChanged
       .subscribe(
         (recipes: Recipe[]) => this.recipes = recipes
       );
@@ -26,6 +28,10 @@ export class RecipeListComponent implements OnInit {
   onNewRecipe() {
     console.log('Activated route: ', this.activatedRoute);
     this.router.navigate(['new'], {relativeTo: this.activatedRoute});
+  }
+
+  ngOnDestroy(): void {
+    this.recipesChanged.unsubscribe();
   }
 
 }
